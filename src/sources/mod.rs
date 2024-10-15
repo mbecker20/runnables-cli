157deb2ae3dbd371a413e4bb420153e85dc37917
mbecker20 <becker.maxh@gmail.com>
2024-10-14 21:20:14 -0400

    more efficient get runincludes / ignores

--- src/sources/mod.rs
+++ src/sources/mod.rs
@@ -22,7 +22,7 @@ pub fn get_runnables(args: &CliArgs) -> anyhow::Result<Vec<Runnable>> {
   let path = PathBuf::from_str(&args.path)?;
 
   let runincludes = get_runincludes(&path);
-  let runignores = get_runignores(&path);
+  let runignores = get_runignores(&path, &runincludes);
 
   let mut runnables = Vec::new();
 
@@ -56,56 +56,65 @@ pub fn run_runnable(runnable: Runnable) {
 
 fn get_runincludes(path: &Path) -> Vec<PathBuf> {
   let mut runincludes = Vec::<PathBuf>::new();
-  runincludes.extend(get_runinclude(path));
-  let Ok(entries) = fs::read_dir(path) else {
-    return runincludes;
-  };
-  for entry in entries.flatten() {
-    if let Ok(metadata) = entry.metadata() {
-      if metadata.is_dir() {
-        let path = entry.path();
-        runincludes.extend(get_runincludes(&path));
-      }
-    }
-  }
+  get_runincludes_inner(path, &mut runincludes);
   runincludes
 }
 
-fn get_runinclude(path: &Path) -> Vec<PathBuf> {
+fn get_runincludes_inner(path: &Path, runincludes: &mut Vec<PathBuf>) {
+  add_runincludes(path, runincludes);
+  let Ok(entries) = fs::read_dir(path) else {
+    return;
+  };
+  for entry in entries.flatten() {
+    if let Ok(metadata) = entry.metadata() {
+      if metadata.is_dir() {
+        get_runincludes_inner(&entry.path(), runincludes)
+      }
+    }
+  }
+}
+
+fn add_runincludes(path: &Path, runincludes: &mut Vec<PathBuf>) {
   let Ok(runinclude) = fs::read_to_string(path.join(".runinclude")) else {
     return Default::default();
   };
-  runinclude
-    .split('\n')
-    .flat_map(|p| path.join(p).canonicalize())
-    .collect()
+  runincludes.extend(
+    runinclude
+      .split('\n')
+      .flat_map(|p| path.join(p).canonicalize()),
+  );
 }
 
-fn get_runignores(path: &Path) -> Vec<PathBuf> {
+fn get_runignores(path: &Path, runincludes: &[PathBuf]) -> Vec<PathBuf> {
   let mut runignores = Vec::<PathBuf>::new();
-  runignores.extend(get_runignore(path));
+  get_runignores_inner(path, &mut runignores, runincludes);
+  runignores
+}
+
+fn get_runignores_inner(path: &Path, runignores: &mut Vec<PathBuf>, runincludes: &[PathBuf]) {
+  add_runignores(path, runignores);
   let Ok(entries) = fs::read_dir(path) else {
-    return runignores;
+    return;
   };
   for entry in entries.flatten() {
     if let Ok(metadata) = entry.metadata() {
       if metadata.is_dir() {
         let path = entry.path();
-        if !ignore_dir(&path) {
-          runignores.extend(get_runignores(&path));
+        if (!runincludes.is_empty() && runincludes.contains(&path)) && !ignore_dir(&path) {
+          get_runignores_inner(&path, runignores, runincludes);
         }
       }
     }
   }
-  runignores
 }
 
-fn get_runignore(path: &Path) -> Vec<PathBuf> {
+fn add_runignores(path: &Path, runignores: &mut Vec<PathBuf>) {
   let Ok(runignore) = fs::read_to_string(path.join(".runignore")) else {
     return Default::default();
   };
-  runignore
-    .split('\n')
-    .flat_map(|p| path.join(p).canonicalize())
-    .collect()
+  runignores.extend(
+    runignore
+      .split('\n')
+      .flat_map(|p| path.join(p).canonicalize()),
+  );
 }
